ae274ffa45ffa09db29d892dd0d600571c9934c1
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-09-23 16:56:53 -0700

    search workin

---
+++ src/Inputs.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Inputs extends Component {
+
+    render() {    
+        console.log(this.state)
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -6,25 +6,48 @@ import PokeList from './PokeList.js'
 export default class SearchPage extends Component {
     state = {
         pokedex: [],
+        sortOrder: 'asc',
+        query:'',
+        isLoading: false,
     }
 
     componentDidMount = async () => {
-        const response = await request.get ('https://pokedex-alchemy.herokuapp.com/api/pokedex?page=1&perPage=150&sort=id&direction=asc')
-        this.setState({pokedex:response.body.results})
+        this.fetch()
+        }
+
+    fetch = async () => {
+        this.setState ({ isLoading : true })
+        const response = await request.get (`https://pokedex-alchemy.herokuapp.com/api/pokedex?page=1&perPage=150&sort=id&direction=${this.state.sortOrder}&pokemon=${this.state.query}`)
+        this.setState ({
+        pokedex: response.body.results,
+        isLoading: false
+        })
     }
+
+    handleChange = (e) => {
+        this.setState({query: e.target.value});
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault();        
+        this.fetch();
+    }
+
     
     render() { 
         console.log(this.state)    
         return (
-            <div>                
+            <div> 
+                <form onSubmit={this.handleSubmit}>
+                <input onChange={this.handleChange} />
+                <button>Search!</button>
+                </form> 
 
-                {/* {
-                this.state.pokedex.length === 0
+                {
+                this.state.isLoading
                 ? <h1>Loading</h1>
-                :this.state.pokedex.map (pokedex => <PokeList pokedex = {this.state.pokedex}
-                    /> ) }       */}
-
-              <PokeList pokedex = {this.state.pokedex} />
+                : <PokeList pokedex = {this.state.pokedex} />
+                }
             </div>
         )
     }
