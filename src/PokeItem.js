440aec58618c4e96c456238d4d86269b5b2ac174
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-09-23 18:30:29 -0700

    styled and drop down #2

--- src/PokeItem.js
+++ src/PokeItem.js
@@ -7,17 +7,27 @@ export default class PokeItem extends Component {
         return (
             <div className = "pokemonCard"><ul>
                 <li>
-                <img src = {this.props.url_image} alt = {this.props.pokemon}/>
-                <h1>{this.props.pokemon}</h1>
-                <p>{this.props.id}</p>
-                  <p> type-1 : {this.props.type_1}</p>
-                  <p>type-2 : {this.props.type_2}</p>
-                  <p>hp : {this.props.hp}</p>
-                  <p>atk : {this.props.attack}</p>
-                  <p>def : {this.props.defense}</p>
-                  <p>sp-atk : {this.props.special_attack}</p>
-                  <p>sp-def : {this.props.special_defense}</p>
-                  <p>spd : {this.props.speed} </p>
+                 <span></span>
+                  <img src = {this.props.url_image} alt = {this.props.pokemon}/>
+                  <span></span>
+
+                  <span></span>
+                  <h1>{this.props.pokemon}</h1>
+                  <span></span>
+
+                  <span></span>
+                  <span>Pokedex Number: {this.props.id}</span>
+                  <span></span>
+                 
+                  <span> type-1 : {this.props.type_1}</span>              
+                  <span>type-2 : {this.props.type_2}</span>
+                  <span>hp : {this.props.hp}</span>
+                  <span>atk : {this.props.attack}</span>
+                  <span>def : {this.props.defense}</span>
+                  <span>spd : {this.props.speed} </span>
+                  <span>sp-atk : {this.props.special_attack}</span>
+                  <span>sp-def : {this.props.special_defense}</span>
+                  <span>weight : {this.props.weight} </span>
                 </li>
                 </ul>
             </div>
