aab480254ad77118c7d7c242677160e72c6564e0
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-09-24 13:02:04 -0700

    in progress: styling for smaller (laptop) screen

--- src/PokeItem.js
+++ src/PokeItem.js
@@ -7,27 +7,22 @@ export default class PokeItem extends Component {
         return (
             <div className = "pokemonCard"><ul>
                 <li>
-                 <span></span>
-                  <img src = {this.props.url_image} alt = {this.props.pokemon}/>
-                  <span></span>
-
-                  <span></span>
-                  <h1>{this.props.pokemon}</h1>
-                  <span></span>
-
-                  <span></span>
-                  <span>Pokedex Number: {this.props.id}</span>
-                  <span></span>
+                  <section className = "statsHeader">
+                    <img src = {this.props.url_image} alt = {this.props.pokemon}/>
+                    <h1>{this.props.pokemon}</h1>
+                    Pokedex Number: {this.props.id}
+                  </section>
                  
-                  <span> type-1 : {this.props.type_1}</span>              
-                  <span>type-2 : {this.props.type_2}</span>
-                  <span>hp : {this.props.hp}</span>
-                  <span>atk : {this.props.attack}</span>
-                  <span>def : {this.props.defense}</span>
-                  <span>spd : {this.props.speed} </span>
-                  <span>sp-atk : {this.props.special_attack}</span>
-                  <span>sp-def : {this.props.special_defense}</span>
-                  <span>weight : {this.props.weight} </span>
+                  <span className = "stats">
+                  <p>type-1 : {this.props.type_1}</p>              
+                  <p>type-2 : {this.props.type_2}</p>  
+                  <p>hp : {this.props.hp}</p>  
+                  <p>atk : {this.props.attack}</p>  
+                  <p>def : {this.props.defense}</p>  
+                  <p>spd : {this.props.speed}</p>  
+                  <p>sp-atk : {this.props.special_attack}</p>  
+                  <p>sp-def : {this.props.special_defense}</p>  
+                  </span>
                 </li>
                 </ul>
             </div>
